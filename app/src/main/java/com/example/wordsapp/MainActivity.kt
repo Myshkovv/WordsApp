4a04df7c5bd2acd315e5d7329a9f0cf198867a52
Mikhail <myshkov07@gmail.com>
2025-04-06 13:24:27 +0300

    L-7 ViewBinding

--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.wordsapp
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.wordsapp.databinding.ActivityLearnWordBinding
+import com.example.wordsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityLearnWordBinding
+    private var _binding: ActivityLearnWordBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityLearnWordBinding most not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityLearnWordBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_learn_word)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_learn_word)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+//        val tvQuestionWord: TextView = findViewById(R.id.tvQuestionWord)
+//
+//        tvQuestionWord.text = "PERPENDIKULAR"
+//        tvQuestionWord.setTextColor(Color.RED)
+//        tvQuestionWord.setTextColor(Color.parseColor("#FDD600"))
+//        tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.textVariantsColor))
+
+        with(binding){
+            tvQuestionWord.text = "AnroidSprint.ru"
+            tvQuestionWord.setTextColor(Color.BLUE)
+
+        }
+
+
+
     }
 }
